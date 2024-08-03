51d85dddc4e48ec67eeaecc395bc80a527b1bd3e
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-08-03 11:15:17 +0500

    Modified Admin panel

--- main/admin.py
+++ main/admin.py
@@ -4,6 +4,12 @@ from django.contrib import admin
 from main.models import Product, Contact, EmailMessage
 
 
+@admin.register(EmailMessage)
+class EmailMessageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'sender', 'subject', 'created_date']
+    list_display_links = ['id', 'name', 'sender']
+    search_fields = ['name', 'sender']
+
+
 admin.site.register(Product)
 admin.site.register(Contact)
-admin.site.register(EmailMessage)
